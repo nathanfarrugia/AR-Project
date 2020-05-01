66c3de94d2fff287c37926ab6f21614eb2e21532
nathanfarrugia <56016961+nathanfarrugia@users.noreply.github.com>
2020-05-01 18:05:13 +0200

    Update event.js

--- event.js
+++ event.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent('markerhandler', {
           const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
           if (aEntity && intersectedElement === aEntity) {
               const scale = aEntity.getAttribute('scale');
-              Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.01);
+              Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
               aEntity.setAttribute('scale', scale);
           }
       });
